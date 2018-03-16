373d65014cbde4daac5efa576b57d48662b7fc4d
vsparrow <khan@vsparrow.com>
2018-03-16 11:32:11 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  
+    # get 'about', to: 'static#about'
+    # Create a route that is mapped to a students' controller index action, the resulting route should be /students
+     # get 'posts/:id', to: 'posts#show'
+     get 'students', to: 'students#index'
 end
